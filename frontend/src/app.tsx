fc3815a4e0d76fa0b2295ba80fd1215809f1d077
Ricky Yuan <itvcanterlot@gmail.com>
2021-04-20 12:00:32 +0100

    Refactored TSX files to a new folder

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,5 +1,6 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+import SearchDropdownList from './UIElements';
 
 class AttributeRow extends React.Component {
     constructor(props) {
@@ -78,103 +79,6 @@ class AttributeList extends React.Component {
                 </table>
             </div>
         )
-    }
-}
-
-class SearchDropdownList extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            showList: false,
-        };
-
-        this.inputRef = React.createRef();
-    }
-
-    onListItemClick = (e) => {
-        e.preventDefault();
-        
-        // Send the selected index to parent
-        this.props.onListSelectionChange(e);
-        // Unfocus from the input box
-        this.blurInput();
-        // Fill in input box with the name of the selected element
-        const inputNode = this.inputRef.current as HTMLInputElement;
-        const selectedItemNode = e.target as HTMLAnchorElement;
-        inputNode.value = selectedItemNode.textContent;
-    }
-
-    renderListElements = () => {
-        if (!this.props.dropdownList || this.props.dropdownList.length == 0) {
-            return (
-                <div className={"dropdown-menu dropdown-custom-text-content" + (this.state.showList ? " d-block" : "")}>
-                    <a className="dropdown-item disabled" data-key="-1" data-index="-1" key="-1" href="#"><i>No content</i></a>
-                </div>
-            )
-        } else {
-            if (Array.isArray(this.props.dropdownList)) {
-                return (
-                    <div className={"dropdown-menu dropdown-custom-text-content" + (this.state.showList ? " d-block" : "")}>
-                        {this.props.dropdownList.map((item, index) => {
-                            <a className="dropdown-item" data-key={index} data-index={index} key={index} href="#" onMouseDown={this.onListItemClick}>{item}</a>
-                        })}
-                    </div>
-                )
-            } else {
-                let itemList = Object.entries(this.props.dropdownList)
-                    .map(([k, v], index) => {
-                        return <a className="dropdown-item" data-key={k} data-index={index} key={k} href="#" onMouseDown={this.onListItemClick}>{v}</a>
-                    })
-                return (
-                    <div className={"dropdown-menu dropdown-custom-text-content" + (this.state.showList ? " d-block" : "")}>
-                        {
-                            itemList
-                        }
-                    </div>
-                );
-            }
-        }
-    }
-
-    onInputFocus = () => {
-        this.props.updateListHandler();
-        this.setState({
-            showList: true
-        });
-        
-        const inputNode = this.inputRef.current as HTMLInputElement;
-        inputNode.select();
-    }
-
-    onInputBlur = () => {
-        this.setState({
-            showList: false
-        });
-    }
-
-    blurInput = () => {
-        const inputNode = this.inputRef.current as HTMLInputElement;
-        inputNode.blur();
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="input-group mb-0">
-                    <div className="input-group-prepend">
-                        <span className="input-group-text">{this.props.prependText}</span>
-                    </div>
-                    <input type="text" className="form-control dropdown-toggle" data-toggle="dropdown" 
-                        placeholder={this.props.placeholder} aria-label="Relation"
-                        onFocus={this.onInputFocus}
-                        onBlur={this.onInputBlur}
-                        ref={this.inputRef}
-                         />
-                </div>
-                {this.renderListElements()}
-            </div>
-        );
     }
 }
 
@@ -326,10 +230,4 @@ async function getAttributeContentFromDatabase(tableIndex) {
         return val.json();
     })
     return response;
-}
-
-function getTableIndexFromElement() {
-    let tableListSelect = document.getElementById("table-list-select-dropdown") as HTMLSelectElement;
-    let tableIndex = parseInt(tableListSelect.options[tableListSelect.selectedIndex].getAttribute("oid"));
-    return tableIndex;
 }
--- frontend/webpack-config.js
+++ frontend/webpack-config.js
@@ -1,6 +1,6 @@
 module.exports = {
     devtool: 'source-map',
-    entry: "./app.tsx",
+    entry: "./src/app.tsx",
     mode: "development",
     output: {
         filename: "./app-bundle.js"
